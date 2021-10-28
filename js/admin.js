db48a93a72eb46aabe7adbfbea42626d5ad1e89e
Pablo Castillo <psc88sdrd@gmail.com>
2021-10-28 01:54:41 -0300

    uso de localstorage y carga inicial de los productos a la tabla

--- js/admin.js
+++ js/admin.js
@@ -10,6 +10,10 @@ let descripcionProducto = document.querySelector("#descripcionProducto");
 let urlForm = document.querySelector("#urlProducto");
 let categoria = document.querySelector("#cagetoriaProducto")
 let formulario = document.querySelector("#formularioProducto");
+let listaProductos = [];
+
+//** carga inicial de la tabla **//
+cargaInicialTabla();
 
 //** Agregado de eventos blur (foco) **//
 codigoForm.addEventListener("blur", () => {validarCodigoProducto(codigoForm)});
@@ -20,10 +24,11 @@ descripcionProducto.addEventListener("blur", () => {validarCampo(descripcionProd
 categoria.addEventListener("blur", () => validarCategoria(categoria))
 formulario.addEventListener("submit", guardarProducto);
 
+//** guardar producto en localstorage y crear tabla **//
 function guardarProducto(e){
   e.preventDefault();
   if(validarGeneralForm()){
-    console.log("aqui creo el producto");   
+    agregarProducto(); 
   }else{
     console.log("aqui no debo hacer nada");
   }
@@ -31,4 +36,41 @@ function guardarProducto(e){
 
 function agregarProducto(){
   let productoNuevo = new Producto(codigoForm.value, cantidadForm.value, nombreProducto.value, descripcionProducto.value, urlForm.value, categoria.value);
+  listaProductos.push(productoNuevo);
+  localStorage.setItem("listaProducto", JSON.stringify(listaProductos));
+  limpiarFormulario();
+  crearFilaProductos(productoNuevo);
+}
+
+function limpiarFormulario() {
+  formulario.reset();
+  codigoForm.className = "form-control";
+  cantidadForm.className = "form-control";
+  urlForm.className = "form-control";
+  nombreProducto.className = "form-control";
+  descripcionProducto.className = "form-control";
+  categoria.className = "form-control";
+}
+
+function crearFilaProductos(itemProducto){
+  let tabla = document.querySelector("#tablaMix");
+  console.log(itemProducto)
+  tabla.innerHTML += `<tr>
+    <th scope="row">${itemProducto.codigo}</th>
+    <td>${itemProducto.producto}</td>
+    <td>${itemProducto.descripcion}</td>
+    <td>${itemProducto.cantidad}</td>
+    <td>${itemProducto.url}</td>
+    <td class="text-center">
+      <button class="btn btn-warning" onclick="prepararEdicion()">Editar</button>
+      <button class="btn btn-warning mt-2" onclick="prepararEdicion()">Borrar</button>
+    </td>
+  </tr>`
+}
+
+function cargaInicialTabla(){
+  listaProductos = JSON.parse(localStorage.getItem("listaProducto")) || [];
+  listaProductos.forEach((itemProducto) => {
+    crearFilaProductos(itemProducto);
+  });
 }
