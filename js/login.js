3407dd5c3c7f7923c99c396e320445a2a0b01e44
Pablo Castillo <psc88sdrd@gmail.com>
2021-11-01 14:59:54 -0300

    Preparacion logica login

---
+++ js/login.js
@@ -0,0 +1,11 @@
+let email = document.querySelector("#inputEmail");
+let pass = document.querySelector("#inputPassword");
+let formularioIngresar = document.querySelector("#formularioLogin");
+
+formularioIngresar.addEventListener = ("submit",validarUsuario);
+
+function validarUsuario() {
+    // e.preventDefault();
+    console.log('escuchando')
+}
+
